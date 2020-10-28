c96f19775d21231c1e11f3ae7e42a4b10abca843
Nathan Tseng <tsnathan@umich.edu>
2020-10-27 20:19:10 -0400

    Add speech analysis

--- src/pages/SpeechTest/speechAnalysis.py
+++ src/pages/SpeechTest/speechAnalysis.py
@@ -1,26 +1,77 @@
-import flask
+#import flask
 import flair
+from flair.models import SequenceTagger
 #pip install flair
 
-app = Flask(__name__)
-filler_words = ["well", "um", "er", "uh", "hmm", "like", "actually", "basically", "seriously", "you see", "you know", "I mean", "you know what I mean", "at the end of the day", "believe me", "I guess", "I suppose", "or something", "right", "mhm", "uh huh"]
+# app = Flask(__name__)
+filler_words = [" well ", " um ", " er ", " uh ", " hmm ", " like ",
+                " actually ", " basically ", " seriously ", " you see ",
+                " you know ", " I mean ",
+                " you know what I mean ",
+                " at the end of the day ", " believe me ", " I guess ", " I suppose ",
+                " or something ", " right ", " mhm ", " uh huh "]
 
 
-@app.route('/api/v1/speechAnalysis/', methods=["GET"])
-def speech_analysis():
-    data = flask.request.json
-    print(data)
-    transcript = data['transcript']
-    word_count = 0
-    for word in filler_words:
-        word_count += transcript.count(word)
-    print(word_count, "filler words were said")
-    
-    flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
-    sentiment = flair.data.Sentence(transcript
-    flair_sentiment.predict(s)
-    total_sentiment = s.labels
-    print(total_sentiment)
+# @app.route('/api/v1/speechAnalysis/', methods=["GET"])
+# def speech_analysis():
+#     data = flask.request.json
+#     print(data)
+#     transcript = data['transcript']
+#     word_count = 0
+#     for word in filler_words:
+#         word_count += transcript.count(word)
+#     print(word_count, "filler words were said")
+#
+#     flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+#     sentiment = flair.data.Sentence(transcript)
+#     flair_sentiment.predict(s)
+#     total_sentiment = s.labels
+#     print(total_sentiment)
+
+
+def speech_analysis_demo():
+    with open("demofile.txt", "r", encoding="cp1252") as f:
+        for line in f:
+            transcript = line.strip()
+            word_count = {}
+            for word in filler_words:
+                word_count[word] = transcript.count(word)
+            for word in filler_words:
+                if word_count[word] != 0:
+                    print("The phrase:", word, "was said", word_count[word], "times")
+            flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+            sentence = flair.data.Sentence(transcript)
+            flair_sentiment.predict(sentence)
+            total_sentiment = sentence.labels
+            tagger = SequenceTagger.load('ner')
+            tagger.predict(sentence)
+            print(total_sentiment)
+            for entity in sentence.get_spans('ner'):
+                print(entity)
+  #  f = open("demofile.txt", "r")
+  #   transcript = f.readline()
+  #   print(transcript)
+  #   word_count = {}
+  #   for word in filler_words:
+  #       word_count[word] = transcript.count(word)
+  #   for word in filler_words:
+  #       if word_count[word] != 0:
+  #           print("The phrase:", word, "was said", word_count[word], "times")
+  #   flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+  #   sentence = flair.data.Sentence(transcript)
+  #   flair_sentiment.predict(sentence)
+  #   total_sentiment = sentence.labels
+  #   tagger = SequenceTagger.load('ner')
+  #   tagger.predict(sentence)
+  #   print(total_sentiment)
+  #   for entity in sentence.get_spans('ner'):
+  #       print(entity)
+
+speech_analysis_demo()
+
+
+
+
 
 
 
